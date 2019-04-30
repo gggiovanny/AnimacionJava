5db00e255ec5a0e759cbcb3e4810d28b8e30b1f6
gggiovanny <giovanny.gonzalez.19@gmail.com>
2019-04-30 10:33:05 -0500

    First commit

---
+++ src/app/Animar.java
@@ -0,0 +1,111 @@
+package app;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class Animar extends JPanel implements Runnable
+{
+	private ImageIcon[] imagen;
+	private ImageIcon imagenActual;
+	private ImageIcon fondo;
+	private int posX;
+	private int posY;
+	private int limiteIzquierdo;
+	private int limiteDerecho;
+	private boolean vaDerecho;
+	
+
+	/**
+	 * Create the panel.
+	 */
+	public Animar()
+	{
+		limiteDerecho = 550;
+		limiteIzquierdo = 210;
+		posX = limiteIzquierdo;
+		posY = 380;
+		vaDerecho = true;
+		fondo = new ImageIcon("img/hombre-fondo.jpg");
+		String[] listaImagenes = new String[25];
+		
+		int c;
+		for (int i = 0; i<listaImagenes.length; i++)
+		{
+			c = i + 1;
+			if(c >= 10)
+			{
+				listaImagenes[i] = "00" + c + ".png";
+			}
+			else
+			{
+				listaImagenes[i] = "000" + c + ".png";
+			}
+		}
+		
+		imagen = new ImageIcon[listaImagenes.length];
+		
+		for (int i = 0; i<imagen.length; i++)
+			imagen[i] = new ImageIcon("img/" + listaImagenes[i]);
+		
+		imagenActual = imagen[0];
+		
+		Thread hilo = new Thread(this);
+		hilo.start();
+	}
+	
+	@Override
+	protected void paintComponent(Graphics gg)
+	{
+		super.paintComponent(gg);
+		Graphics2D g2d = (Graphics2D)gg;
+		
+		
+		g2d.drawImage(fondo.getImage(), 0, 0, this);
+		if(vaDerecho)
+			g2d.drawImage(imagenActual.getImage(), posX, posY, imagenActual.getIconWidth(), imagenActual.getIconHeight(), this);
+		else
+			g2d.drawImage(imagenActual.getImage(), posX + imagenActual.getIconWidth(), posY, -imagenActual.getIconWidth(), imagenActual.getIconHeight(), this);
+//		gg.setColor(Color.RED);
+//		gg.drawLine(limiteIzquierdo, posY + imagenActual.getIconHeight(), limiteDerecho, posY + imagenActual.getIconHeight());
+	}
+	
+	@Override
+	public void run()
+	{
+		int delta = 4;
+		
+		while(true)
+		{
+			for (int i = 0; i<imagen.length; i++)
+			{
+				imagenActual = imagen[i];
+				repaint();
+				try
+				{
+					Thread.sleep(75);
+				} catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+				
+				if(posX >= limiteDerecho - imagenActual.getIconWidth())
+					vaDerecho = false;
+				if(posX <= limiteIzquierdo)
+					vaDerecho = true;
+				
+				if(vaDerecho)
+					posX += delta;
+				else
+					posX -= delta;
+			}
+		}
+	}
+
+}
---
+++ src/app/JApp.java
@@ -0,0 +1,53 @@
+package app;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class JApp extends JFrame
+{
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					JApp frame = new JApp();
+					frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JApp()
+	{
+		setTitle("Un Hombre Caminando xd");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 825, 650);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		Animar animacion = new Animar();
+		contentPane.add(animacion);
+	}
+
+}
